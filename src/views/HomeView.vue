54892f65db145e9a1842f085d65aec3d9e6f304a
Regine <nessagine17@gmail.com>
2025-04-07 13:30:45 +0800

    fix: add text

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,6 +1,11 @@
 <template>
   <div>
     <h1>Welcome to Home View</h1>
+    <p>
+      WELCOME TO E-HYDRO SITE .... Lorem ipsum dolor sit amet consectetur adipisicing elit. Debitis
+      laudantium eos doloremque vitae ducimus. Asperiores sit dolores dolore assumenda veniam, nihil
+      saepe nisi aspernatur quia esse nam consequatur amet recusandae!
+    </p>
   </div>
 </template>
 
