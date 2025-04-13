458a4df5d87428a87f0db2c95f39e288f84840e6
sandrineacibar <sandrineacibar19@gmail.com>
2025-04-13 20:55:53 +0800

    uninstalling sass

--- src/components/NavigationBar.vue
+++ src/components/NavigationBar.vue
@@ -103,7 +103,7 @@ export default {
 </script>
 
 <!--<style lang="scss" scoped>-->
-<style scoped>
+<!--<style scoped>
 header {
   /*background-color: rgba(0, 0, 0, 0.8);*/
   z-index: 99;
@@ -228,5 +228,131 @@ header {
     }
   }
 }
-</style>
+</style>-->
 
+<style scoped>
+header {
+  /*background-color: rgba(0, 0, 0, 0.8);*/
+  z-index: 99;
+  width: 100%;
+  position: fixed;
+  transition: 0.5s ease all;
+  color: #04448d;
+}
+  nav {
+    display: flex;
+    flex-direction: row;
+    padding: 12px 0;
+    transition: 0.5s ease all;
+    width: 90%;
+    margin: 0 auto;
+    @media (min-width: 1140px) {
+      max-width: 1140px;
+    }
+  }
+    ul,
+    .link {
+      font-weight: 400;
+      font-family: 'Inter', sans-serif;
+      color: #04448d;
+      list-style: none;
+      text-decoration: none;
+    }
+
+    li {
+      /*text-transform: uppercase;*/
+      padding: 16px;
+      margin-left: 24px;
+
+      .last,
+      second-last {
+        margin-left: -2.7rem !important;
+      }
+      .last {
+        font-size: 25px;
+        border-style: none;
+      }
+      .second-last {
+        font-size: 22px;
+        border-style: none;
+      }
+    }
+
+    .link {
+      font-size: 18px;
+      transition: 0.5s ease all;
+      /*padding-bottom: 4px;*/
+      border-bottom: 1px solid transparent;
+      &:hover {
+        color: #02adef;
+        border-color: #02adef;
+      }
+    }
+
+    .branding {
+      display: flex;
+      align-items: center;
+      /*margin-left: -2rem !important;*/
+
+      .first-word,
+      .second-word {
+        font-weight: 500;
+        font-size: 53px;
+        transition: 0.5s ease all;
+        font-family: 'Antonio', sans-serif;
+      }
+      .first-word {
+        color: #02adef;
+      }
+      .second-word {
+        color: #04448d;
+      }
+    }
+    .navigation {
+      display: flex;
+      align-items: center;
+      flex: 1;
+      justify-content: flex-end;
+    }
+
+    .icon-style {
+      font-size: 50px;
+      display: flex;
+      align-items: center;
+      position: absolute;
+      top: 0;
+      right: 24px;
+      height: 100%;
+      margin-top: 4px;
+      cursor: pointer;
+      transition:
+        transform 0.5s ease,
+        opacity 0.4s ease;
+    }
+
+    .icon-style.icon-active {
+      transform: rotate(180deg);
+    }
+
+    .dropdown-nav {
+      display: flex;
+      flex-direction: column;
+      position: fixed;
+      width: 90%;
+      max-width: 250px;
+      height: 100%;
+      backdrop-filter: blur(10px);
+      left: 0;
+      top: 0;
+      margin: auto;
+      padding: 12px 0;
+      li {
+        .link {
+          color: #04448d;
+          font-size: 18px;
+        }
+      }
+    }
+
+
+</style>
